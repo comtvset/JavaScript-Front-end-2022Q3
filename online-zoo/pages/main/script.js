ca3427daa58dcc5997f97364d156af3d2bfb4314
Vadzim Tsikhaniuk <comtvset@gmail.com>
2022-10-09 21:50:40 +0200

    feat: carusel for commit all resolutions

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -49,13 +49,26 @@ document.querySelector('.right-btn').addEventListener('click', function() {
 });
 
 
-// let offset = 0;
-// const petsContainer = document.querySelector('.pets-container');
 
-// document.querySelector('.right-btn').addEventListener('click', function() {
-//     offset = offset + 366;
-//     if(offset > 1100) {
+
+// let offset = 0;
+// const petsContainer = document.querySelector('.slider-line');
+
+// document.querySelector('.slider').addEventListener('click', function() {
+//     offset = offset + 298;
+//     if(offset > 2300) {
 //         offset = 0
 //     }
 //     petsContainer.style.left = -offset + 'px';
 // });
+
+let range = document.querySelector('.slider');
+let block = document.querySelector('.slider-line');
+let sliderComment = document.querySelector('.slider-comment');
+range.oninput = function () {
+    if(sliderComment.offsetWidth == 940) {
+        block.style.right = this.value *321 + 'px';
+        console.log(sliderComment.offsetWidth)
+    }
+    else {block.style.right = this.value *295.5 + 'px'};
+}
