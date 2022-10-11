cebb601072105eb994a084245bc70829c3e46134
Vadzim Tsikhaniuk <comtvset@gmail.com>
2022-10-11 02:35:06 +0200

    feat: burger-menu pages main/donate

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,1 +1,43 @@
-"use strict"
+const fbMenu = document.querySelector('.fbmenu');
+const boxMenu = document.querySelector('.menu__box');
+const logo = document.querySelector('.logo');
+const main = document.querySelector('.main');
+const introOp = document.querySelector('.intro');
+const imgOp = document.querySelector('.img-puppy');
+const aboutOp = document.querySelector('.wrapper_about-1');
+
+if (fbMenu) {
+    // const boxMenu = document.querySelector('.menu__box');
+    fbMenu.addEventListener('click', activeBurger);
+    boxMenu.addEventListener('click', deactiveBurger);
+
+}
+const about = document.getElementById('menu');
+logo.addEventListener('click', deactiveBurger);
+main.addEventListener('click', deactiveBurger);
+
+// about.addEventListener('click', function() {
+//     boxMenu.classList.remove('_active')
+// });
+
+function activeBurger() {
+    document.body.classList.toggle('_lock');
+    fbMenu.classList.toggle('_active');
+    boxMenu.classList.toggle('_active');
+    document.body.classList.toggle('_active');
+    // logo.classList.toggle('_active');
+    introOp.classList.toggle('_active');
+    imgOp.classList.toggle('_active');
+    aboutOp.classList.toggle('_active');
+}
+
+function deactiveBurger() {
+    document.body.classList.remove('_lock');
+    boxMenu.classList.remove('_active');
+    fbMenu.classList.remove('_active');
+    document.body.classList.remove('_active');
+    // logo.classList.remove('_active');
+    introOp.classList.remove('_active');
+    imgOp.classList.remove('_active');
+    aboutOp.classList.remove('_active');
+}
